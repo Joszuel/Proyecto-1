bc6a72040e2ba6466507a9a74759e397e3adb706
Joszden <joszden@gmail.com>
2024-12-09 03:06:29 -0400

    Arreglo de js condicionales activadores de efecto de los botones

--- app.js
+++ app.js
@@ -3,25 +3,26 @@ let pag = document.getElementsByClassName("pag");
 
 window.onscroll = function() {
 
-    if (window.scrollY <= window.innerHeight){
+    console.log(window.scrollY)
+    if (window.scrollY <= window.innerHeight*0.5){
         boton[0].classList.add("activado");
         boton[1].classList.remove("activado");
         boton[2].classList.remove("activado");
         boton[3].classList.remove("activado");
     }
-    if (window.scrollY >= window.innerHeight && window.scrollY < window.innerHeight*2){
+    if (window.scrollY >= window.innerHeight*0.5 && window.scrollY < window.innerHeight*1.5){
         boton[0].classList.remove("activado");
         boton[1].classList.add("activado");
         boton[2].classList.remove("activado");
         boton[3].classList.remove("activado");
     }
-    if (window.scrollY >= window.innerHeight*2 && window.scrollY < window.innerHeight*3){
+    if (window.scrollY >= window.innerHeight*1.5 && window.scrollY < window.innerHeight*2.5){
         boton[0].classList.remove("activado");
         boton[1].classList.remove("activado");
         boton[2].classList.add("activado");
         boton[3].classList.remove("activado");
     }
-    if (window.scrollY >= window.innerHeight*3 && window.scrollY < window.innerHeight*4){
+    if (window.scrollY >= window.innerHeight*2.5 && window.scrollY < window.innerHeight*3.5){
         boton[0].classList.remove("activado");
         boton[1].classList.remove("activado");
         boton[2].classList.remove("activado");
@@ -30,23 +31,14 @@ window.onscroll = function() {
 };
 
 boton[0].addEventListener("click", ()=>{
-    pag[0].scrollIntoView({
-        behavior: 'smooth'
-    })
+    pag[0].scrollIntoView({behavior: 'smooth'})
 });
-
-boton[1].addEventListener("click", ()=>{
-    pag[1].scrollIntoView({
-        behavior: 'smooth'
-    })
+boton[1].addEventListener("click", ()=>{ 
+    pag[1].scrollIntoView({behavior: 'smooth'})
 });
 boton[2].addEventListener("click", ()=>{
-    pag[2].scrollIntoView({
-        behavior: 'smooth'
-    })
+    pag[2].scrollIntoView({behavior: 'smooth'})
 });
 boton[3].addEventListener("click", ()=>{
-    pag[3].scrollIntoView({
-        behavior: 'smooth'
-    })
+    pag[3].scrollIntoView({behavior: 'smooth'})
 });
