35fa0489696becd7b44e7a38fa847982610d275d
Joszden <joszden@gmail.com>
2024-12-09 04:29:54 -0400

    Final, resulta que estamaba mal el navegador

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
 let boton = document.getElementsByClassName("bot");
 let pag = document.getElementsByClassName("pag");
 
-window.onscroll = function() {
-
-    console.log(window.scrollY)
+window.addEventListener("scroll", ()=>{
     if (window.scrollY <= window.innerHeight*0.5){
         boton[0].classList.add("activado");
         boton[1].classList.remove("activado");
         boton[2].classList.remove("activado");
         boton[3].classList.remove("activado");
     }
-    if (window.scrollY >= window.innerHeight*0.5 && window.scrollY < window.innerHeight*1.5){
+    if (window.scrollY > window.innerHeight*0.5 && window.scrollY < window.innerHeight*1.5){
         boton[0].classList.remove("activado");
         boton[1].classList.add("activado");
         boton[2].classList.remove("activado");
         boton[3].classList.remove("activado");
     }
-    if (window.scrollY >= window.innerHeight*1.5 && window.scrollY < window.innerHeight*2.5){
+    if (window.scrollY > window.innerHeight*1.5 && window.scrollY < window.innerHeight*2.5){
         boton[0].classList.remove("activado");
         boton[1].classList.remove("activado");
         boton[2].classList.add("activado");
         boton[3].classList.remove("activado");
     }
-    if (window.scrollY >= window.innerHeight*2.5 && window.scrollY < window.innerHeight*3.5){
+    if (window.scrollY > window.innerHeight*2.5 && window.scrollY < window.innerHeight*3.5){
         boton[0].classList.remove("activado");
         boton[1].classList.remove("activado");
         boton[2].classList.remove("activado");
         boton[3].classList.add("activado");
     }
+});
+
+window.onscroll = function() {
+
+    console.log(window.scrollY)
+    
 };
 
 boton[0].addEventListener("click", ()=>{
